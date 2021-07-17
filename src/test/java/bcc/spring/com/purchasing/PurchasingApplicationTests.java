1c9548c06f932b51dfccdf735eec120856f792f3
bryan2459 <mvs390@gmail.com>
2021-07-17 12:00:02 +1000

    Initial commit

---
+++ src/test/java/bcc/spring/com/purchasing/PurchasingApplicationTests.java
@@ -0,0 +1,13 @@
+package bcc.spring.com.purchasing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
